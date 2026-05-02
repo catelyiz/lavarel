191e0d55acb550b1c63993ce0f21755dcece98dd
catelyiz <vajeancha@gmail.com>
2026-05-02 20:37:30 +0700

    laravel baru

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <h1>Welcome</h1>
+    <a href="/fakultas">List Fakultas</a>
+    <a href="/fakultas/edit">Edit Fakultas</a>
+    <a href="/fakultas/show">Detail Fakultas</a>
+</x-layout>
